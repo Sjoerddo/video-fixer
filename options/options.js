6cf27ca01f0cdd77472af1239dc2b4c2b89f71be
Sjoerd <s.weerstra@student.fontys>
2018-01-14 21:56:57 +0100

    Handle radio and checkbox options dynamically

--- options/options.js
+++ options/options.js
@@ -1,25 +1,18 @@
 const OPTIONS_KEY = 'options';
-const Form = document.forms.options;
 const InputTypes = { radio: 'value', checkbox: 'checked' };
 const ValueTypes = { string: 'value', boolean: 'checked' };
 
 document.addEventListener('DOMContentLoaded', () => {
-    addEvent('input[type="radio"][name="alignment"]', ['click', saveInputTarget]);
-    addEvent('#close-after-ending', ['click', saveInputTarget]);
+    const form = document.forms.options;
 
-    const options = getOptions(OPTIONS_KEY) || {};
-    Object.entries(options).forEach(([key, value]) => {
-        Form[key][ValueTypes[typeof value]] = value;
+    form.querySelectorAll('input[type=radio], input[type=checkbox]').forEach((input) => {
+        input.addEventListener('click', saveInputTarget);
+    });
+
+    Object.entries(getOptions(OPTIONS_KEY)).forEach(([key, value]) => {
+        form[key][ValueTypes[typeof value]] = value;
     });
 });
-
-const addEvent = (selector, [name, listener]) => {
-    const elements = document.querySelectorAll(selector);
-
-    elements.forEach((element) => {
-        element.addEventListener(name, listener);
-    });
-};
 
 const saveInputTarget = ({ target }) => {
     const type = InputTypes[target.type];
@@ -27,7 +20,7 @@ const saveInputTarget = ({ target }) => {
 };
 
 const save = (obj) => {
-    const options = getOptions(OPTIONS_KEY) || {};
+    const options = getOptions(OPTIONS_KEY);
 
     Object.entries(obj).forEach(([key, value]) => {
         options[key] = value;
@@ -37,7 +30,7 @@ const save = (obj) => {
 };
 
 const getOptions = (key) => {
-    return JSON.parse(localStorage.getItem(key));
+    return JSON.parse(localStorage.getItem(key)) || {};
 };
 
 const setOptions = (key, options) => {
